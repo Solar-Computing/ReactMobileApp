356f47cef2567974e62e57756b1cb28d1f0fa2d1
TommasoPieroncini <tommipier96@hotmail.it>
2016-10-15 14:02:40 -0400

    Tommy's branch first commit

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 /**
  * Sample React Native App
- * https://github.com/facebook/react-native
+ * https:github.com/facebook/react-native
  * @flow
  */
 
@@ -10,52 +10,126 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  ListView,
+  ScrollView,
+  Switch
 } from 'react-native';
+var Accordion = require('react-native-accordion')
+var roomsData = [
+  {name: "Kitchen", sockets: [
+    {name: 'Lights', state: {switchIsOn: false}},
+    {name: 'Fridge', state: {switchIsOn: false}}]},
+  {name: "Living Room", sockets: [
+    {name: 'Light 1', state: {switchIsOn: true}},
+    {name: 'Light 2', state: {switchIsOn: true}}]}]
 
 class AwesomeProject extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <LotsOfGreetings names='Aayush' />
-        <LotsOfGreetings names='Leonie' />
-        <LotsOfGreetings names='Sujeeth' />
-        <LotsOfGreetings names='Tommy' />
+      <ScrollView style={null}>
+        <ListOfRooms data={roomsData}></ListOfRooms>
+      </ScrollView>
+    );
+  }
+}
+
+class ListOfRooms extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(this.props.data),
+    };
+  }
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderCollapsibleRow}
+      />
+    );
+  }
+  renderCollapsibleRow(rowData) {
+    var header = (
+      <View style={styles.headerView}>
+        <Text style={styles.headerText}>{rowData.name}</Text>
       </View>
     );
-  }
-}
 
-class LotsOfGreetings extends Component {
-  render() {
-//     let pic = {
-//       uri: 'http://frostney.github.io/talks/react-native/slides/images/meme2.jpg'
-//       uri: 'http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg'
-//     };
+    var content = (
+      <RoomOptions data={rowData.sockets}></RoomOptions>
+    );
+ 
     return (
-//       <Image source={pic} style={{width: 193, height: 11}}/>
-      <Text>Hello {this.props.names}!</Text>
+      <Accordion
+        header={header}
+        content={content}
+      />
     );
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
+class RoomOptions extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(this.props.data),
+    };
+  } render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderOptionRow}
+        />
+      );
+    }
+    renderOptionRow(rowData) {
+      return (
+        <View style={styles.contentView}>
+          <View><Text style={styles.contentText}>{rowData.name}</Text></View>
+          <View style={{flex: 1, flexDirection: 'row', padding: 20, justifyContent: 'flex-end'}}><OnOffSwitch state={rowData.state}></OnOffSwitch></View>
+        </View>
+        );
+    }
+  }
+
+class OnOffSwitch extends Component {
+  constructor(props) {
+    super(props)
+    this.state = this.props.state
+  }
+  render() {
+    return (
+      <View>
+        <Switch
+          onValueChange={(value) => this.setState({switchIsOn: value})}
+          value={this.state.switchIsOn}
+        />
+      </View>
+          );
+  }
+}
+
+const styles = StyleSheet.create({
+ headerText: {
+  fontSize: 25,
+  fontWeight: 'bold'
+ },
+ headerView: {
+  alignItems: 'center',
+  padding: 25,
+  backgroundColor: '#D3D3D3',
+ },
+ contentText: {
+  padding: 20,
+  fontSize: 20,
+ },
+ contentView: {
+  backgroundColor: '#FFFFFF',
+  flex: 1,
+  flexDirection: 'row',
+ }
+});
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
