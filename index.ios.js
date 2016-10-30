530da67a214e7f671c91188d5a901e27b4599cfa
leoniereif <leonie.reif@gatech.edu>
2016-10-30 15:05:43 -0400

    prepared merge with master

--- index.ios.js
+++ index.ios.js
@@ -2,22 +2,33 @@ import React, {Component} from 'react';
 import {
   AppRegistry,
   View,
+  Text,
+  StyleSheet,
   ScrollView
 } from 'react-native';
 import DataList from './layout/feed.js';
 import Navigation from './layout/navigation.js';
+import Swiper from 'react-native-swiper';
+import SettingsPage from './layout/SettingsPage.js';
+import styles from './layout/styles.js';
 
 class Home extends Component {
   render() {
     return (
-        <ScrollView>
-            <View>
-                <Navigation></Navigation>
-            </View>
-            <ScrollView>
-                <DataList></DataList>
-            </ScrollView>
-        </ScrollView>
+      <View>
+        <Navigation></Navigation>
+        <Swiper style={styles.wrapper} loop={false}>
+          <View style={styles.slide1}>
+            <SettingsPage></SettingsPage>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Sujeeths Page</Text>
+          </View>
+          <View style={styles.slide3}>
+            <DataList></DataList>
+          </View>
+        </Swiper>
+      </View>
     );
   }
 }
--- layout/styles.js
+++ layout/styles.js
@@ -3,58 +3,23 @@ import {
 } from 'react-native';
 
 export default StyleSheet.create({
-    /*
-    * Navigation Bar
-    */
-    navbar: {
-        backgroundColor: '#2c3239',
-        paddingTop: 20,
-        paddingBottom: 1,
-        justifyContent: 'center',
-        flexDirection: 'row'
+    wrapper: {
     },
-    navBarImage: {
-        width: 40,
-        height: 40,
-        justifyContent: 'center',
-        alignItems: 'center'
+    slide1: {
+      flex: 1,
     },
-    statusbar: {
-        backgroundColor: '#191f28',
-        paddingTop: 3,
-        paddingBottom: 3,
-        justifyContent: 'center',
-        flexDirection: 'row'
+    slide2: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#97CAE5',
     },
-    statusbarTitles: {
-        color: '#F5FCFF',
-        justifyContent: 'center',
-        //paddingLeft: 40,
-        textAlign: 'center',
-        flex: 1
+    slide3: {
+      flex: 1,
     },
-
-    /*
-    *   Feed
-    */
-    rowSeparator: {
-        backgroundColor: 'rgba(0,0,0,0.1)',
-        height: 1,
-        marginLeft: 4
-    },
-    rowSeparatorHighlighted: {
-        opacity: 0.0
-    },
-    cellContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#FFF',
-        padding: 4
-    },
-    cellImage: {
-        height: 80,
-        width: 60,
-        marginRight: 8,
-        resizeMode: 'contain'
-    }
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
+  }
 });
