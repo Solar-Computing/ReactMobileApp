07f13ebd706a89aa69f30751325315c4c0ed0e51
Aayush Kumar <aayush.kumarmail@gmail.com>
2016-11-01 10:59:06 -0400

    created a wrapper app.js file

--- index.android.js
+++ index.android.js
@@ -6,28 +6,13 @@ import {
   StyleSheet,
   ScrollView
 } from 'react-native';
-import DataList from './layout/feed.js';
-import Navigation from './layout/navigation.js';
-import Swiper from 'react-native-swiper';
-import SettingsPage from './layout/SettingsPage.js';
-import styles from './layout/styles.js';
+import App from './App.js';
 
 class Home extends Component {
   render() {
     return (
       <View>
-        <Navigation></Navigation>
-        <Swiper style={styles.wrapper} loop={false}>
-          <View style={styles.slide1}>
-            <SettingsPage></SettingsPage>
-          </View>
-          <View style={styles.slide2}>
-            <Text style={styles.text}>Sujeeths Page</Text>
-          </View>
-          <View style={styles.slide3}>
-            <DataList></DataList>
-          </View>
-        </Swiper>
+        <App></App>
       </View>
     );
   }
